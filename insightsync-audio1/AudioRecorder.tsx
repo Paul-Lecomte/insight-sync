6b437af7f92cb025933be23764346a564962833c
Paul Lecomte <164857566+Paul-Lecomte@users.noreply.github.com>
2025-04-16 09:34:03 +0200

    state

--- insightsync-audio1/AudioRecorder.tsx
+++ insightsync-audio1/AudioRecorder.tsx
@@ -6,6 +6,14 @@ const AudioRecorder: React.FC = () => {
     const mediaRecorderRef = useRef<MediaRecorder | null>(null);
     const audioChunksRef = useRef<Blob[]>([]);
 
+    const startRecording = async () => {
+        const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+        const mediaRecorder = new MediaRecorder(stream);
+        mediaRecorderRef.current = mediaRecorder;
+        audioChunksRef.current = [];
+        mediaRecorder.start();
+        setIsRecording(true);
+    };
 
     return <div>Audio Recorder</div>;
 };
