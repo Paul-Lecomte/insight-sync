6ef5b95c5a17e24de190767c9c421658ea999efa
Paul Lecomte <164857566+Paul-Lecomte@users.noreply.github.com>
2025-04-16 09:32:17 +0200

    state

--- insightsync-audio1/AudioRecorder.tsx
+++ insightsync-audio1/AudioRecorder.tsx
@@ -2,6 +2,7 @@ import React, { useState, useRef } from "react";
 
 const AudioRecorder: React.FC = () => {
     const [isRecording, setIsRecording] = useState(false);
+    const [transcription, setTranscription] = useState("");
 
 
     return <div>Audio Recorder</div>;
